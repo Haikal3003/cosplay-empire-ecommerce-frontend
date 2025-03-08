3553a855a189e905c99f7cbe0faa4819a58136dc
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-03-08 16:34:22 +0700

    add feature delete product

--- src/components/admin/products/ProductTable.tsx
+++ src/components/admin/products/ProductTable.tsx
@@ -1,13 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import { Product } from '../../../utils/types';
 import { usePrefix } from '../../../hooks/usePrefix';
-import { FaEye, FaEdit, FaTrash } from 'react-icons/fa'; // Import ikon dari react-icons
+import { FaEye, FaEdit, FaTrash, FaGalacticSenate } from 'react-icons/fa'; // Import ikon dari react-icons
+import { useState } from 'react';
+import api from '../../../utils/api';
+import DeleteConfirmationModal from '../../modal/DeleteConfirmationModal';
 
 interface Props {
   products: Product[];
 }
 
 export default function ProductTable({ products }: Props) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [productToDelete, setProductToDelete] = useState<Product | null>(null);
+
   const navigate = useNavigate();
   const prefix = usePrefix();
 
@@ -34,33 +40,70 @@ export default function ProductTable({ products }: Props) {
     },
   ];
 
+  const openModal = (product: Product) => {
+    setProductToDelete(product);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleDeleteProduct = async (id: string) => {
+    try {
+      const response = await api.delete(`/products/${id}`);
+
+      if (response) {
+        console.log(response.data.message);
+        setIsModalOpen(false);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleActionClick = (action: string, product: Product) => {
+    if (action === 'detail') {
+      navigate(`${prefix}/products/detail/${product.id}`);
+    } else if (action === 'edit') {
+      navigate(`${prefix}/products/edit/${product.id}`);
+    } else if (action === 'delete') {
+      openModal(product);
+    }
+  };
+
   return (
-    <table className="w-full table-auto border border-slate-300 table-layout-auto">
-      <thead>
-        <tr>
-          {tableHeaders.map((header, index) => (
-            <th key={index} className={`font-semibold text-xs uppercase p-4 border-r border-b border-slate-300 bg-slate-200 ${index === 0 ? 'text-center' : 'text-left'}`}>
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {products?.map((product: Product, index: number) => (
-          <tr key={product.id} className="border-b border-slate-300">
-            <td className="px-4 py-2 border-r border-slate-300 text-center w-[50px]">{index + 1}</td>
-            <td className="px-4 py-2 border-r border-slate-300 w-[530px]">{product.name}</td>
-            <td className="px-4 py-2 border-r border-slate-300">Rp {product.price}</td>
-            <td className="px-4 py-2 space-x-4">
-              {actionButtons.map((action, index) => (
-                <button key={index} className={`text-white py-2 px-3 rounded-md ${action.color} hover:${action.hoverColor} cursor-pointer `}>
-                  {action.icon}
-                </button>
-              ))}
-            </td>
+    <>
+      <table className="w-full table-auto border border-slate-300 table-layout-auto">
+        <thead>
+          <tr>
+            {tableHeaders.map((header, index) => (
+              <th key={index} className={`font-semibold text-xs uppercase p-4 border-r border-b border-slate-300 bg-slate-200 ${index === 0 ? 'text-center' : 'text-left'}`}>
+                {header}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {products?.map((product: Product, index: number) => (
+            <tr key={product.id} className="border-b border-slate-300">
+              <td className="px-4 py-2 border-r border-slate-300 text-center w-[50px]">{index + 1}</td>
+              <td className="px-4 py-2 border-r border-slate-300 w-[530px]">{product.name}</td>
+              <td className="px-4 py-2 border-r border-slate-300">Rp {product.price}</td>
+              <td className="px-4 py-2 space-x-4">
+                {actionButtons.map((action, index) => (
+                  <button key={index} className={`text-white py-2 px-3 rounded-md ${action.color} hover:${action.hoverColor} cursor-pointer `} onClick={() => handleActionClick(action.name, product)}>
+                    {action.icon}
+                  </button>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <DeleteConfirmationModal product={productToDelete} isOpen={isModalOpen} onClose={closeModal} onConfirm={(id) => handleDeleteProduct(id)} />
+    </>
   );
 }
--- src/components/modal/DeleteConfirmationModal.tsx
+++ src/components/modal/DeleteConfirmationModal.tsx
@@ -1,3 +1,29 @@
-export default function DeleteConfirmationModal() {
-  return <div>DeleteConfirmationModal</div>;
+import { Product } from '../../utils/types';
+
+interface Props {
+  product: Product | null;
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: (id: string) => void;
+}
+
+export default function DeleteConfirmationModal({ product, isOpen, onClose, onConfirm }: Props) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white p-6 rounded-md w-96">
+        <p>Are you sure you want delete this product?</p>
+        <p>"{product?.name}"</p>
+        <div className="mt-4 flex justify-end space-x-4">
+          <button className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400" onClick={onClose}>
+            Batal
+          </button>
+          <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => product && onConfirm(product.id)}>
+            Hapus
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
